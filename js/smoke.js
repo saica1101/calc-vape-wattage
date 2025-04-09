e1fb4f9fda3bd9e1e4c7a32a8f8cdc9a4a7bda4e
saica94 <dosracc6111@gmail.com>
2025-04-09 09:17:22 +0900

    煙のエフェクトを追加: マウスムーブメントに応じて煙を生成する機能を実装

---
+++ js/smoke.js
@@ -0,0 +1,16 @@
+let smoke = document.getElementById("smoke");
+
+function createSmoke(e){
+    let elem = document.createElement("div");
+    elem.classList.add("elem");
+
+    elem.style.left = `${e.clientX}px`;
+    elem.style.top = `${e.clientY}px`;
+    smoke.appendChild(elem);
+
+    elem.addEventListener("animationend", () => {
+        elem.remove();
+    })
+}
+
+document.addEventListener("mousemove", createSmoke);
